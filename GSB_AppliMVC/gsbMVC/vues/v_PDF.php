cf88a1c9dcc39e1cf69eb7e618b1f4d8e36dd9c4
Orvuss <77148361+Orvuss@users.noreply.github.com>
2021-10-07 11:37:50 +0200

    PDF juste mise en page pas fonctionnelle

--- GSB_AppliMVC/gsbMVC/vues/v_PDF.php
+++ GSB_AppliMVC/gsbMVC/vues/v_PDF.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
     include "../fpdf/fpdf.php";
-    include "../include/class.pdogsb.inc.php";
-    $bdd = new PDO('mysql:host=172.16.203.210;dbname=gsb_frais_structure;charset=utf8', 'sio', 'slam');
+    include "../include/fonctionCo.php";
+    $bdd = connexion();
     if($bdd){
         $user = $_SESSION['idVisiteur'];
     
@@ -19,47 +19,70 @@
 
         $PDF->SetTextColor(0,0,0);
 
-       
 
         $PDF->SetFont("Arial","B",16);
         $PDF->SetY($position-8);
         $PDF->SetX(15);
-        $PDF->MultiCell(60,8,utf8_decode("Date"),1,'C');
+        $PDF->MultiCell(60,8,utf8_decode("Id"),1,'C');
+
+        $PDF->SetY($position-8);
+        $PDF->SetX(25);
+        $PDF->MultiCell(60,8,utf8_decode("Id du visiteur"),1,'C');
+
+        $PDF->SetY($position-8);
+        $PDF->SetX(50);
+        $PDF->MultiCell(60,8,utf8_decode("Mois"),1,'C');
 
         $PDF->SetY($position-8);
         $PDF->SetX(75);
         $PDF->MultiCell(60,8,utf8_decode("Libellé"),1,'C');
 
         $PDF->SetY($position-8);
-        $PDF->SetX(135);
+        $PDF->SetX(100);
+        $PDF->MultiCell(60,8,utf8_decode("Date"),1,'C');
+
+        $PDF->SetY($position-8);
+        $PDF->SetX(125);
         $PDF->MultiCell(60,8,utf8_decode("Montant"),1,'C');
 
         $PDF->SetTextColor(0,0,0);
 
-    
-	    
-        while ($donne = $requete2->fetch()) {
+
+        $donne = $requete2->fetch();
             
-            $id = $donne['idVisiteur'];
-            $select = $bdd->query("SELECT * FROM LigneFraisHorsForfait WHERE idVisiteur = '$id';");
-            $donneesProduit = $select->fetch();
-            $PDF->SetFont("Arial","I",16);
+        
+        $id = $donne['id'];
+        $select = $bdd->query("SELECT * FROM LigneFraisHorsForfait WHERE id = 1;");
+        $donneesProduit = $select->fetch();
+        $PDF->SetFont("Arial","I",16);
 
-            $PDF->SetY($position);
-            $PDF->SetX(15);
-            $PDF->MultiCell(60,8,utf8_decode($donneesProduit['date']),1,'C');
+        $PDF->SetY($position);
+        $PDF->SetX(15);
+        $PDF->MultiCell(60,8,utf8_decode($donneesProduit['id']),1,'C');
 
-            $PDF->SetY($position);
-            $PDF->SetX(75);
-            $PDF->MultiCell(60,8,utf8_decode($donneesProduit['libelle']."e"),1,'C');
+        $PDF->SetY($position);
+        $PDF->SetX(25);
+        $PDF->MultiCell(60,8,utf8_decode($donneesProduit['idVisiteur']),1,'C');
 
-            $PDF->SetY($position);
-            $PDF->SetX(135);
-            $PDF->MultiCell(60,8,utf8_decode($donne['montant']),1,'C');
+        $PDF->SetY($position);
+        $PDF->SetX(50);
+        $PDF->MultiCell(60,8,utf8_decode($donne['mois']),1,'C');
 
-            $position += 8;
-            
-        }
+        $PDF->SetY($position);
+        $PDF->SetX(75);
+        $PDF->MultiCell(60,8,utf8_decode($donneesProduit['libelle']),1,'C');
+
+        $PDF->SetY($position);
+        $PDF->SetX(100);
+        $PDF->MultiCell(60,8,utf8_decode($donneesProduit['date']),1,'C');
+
+        $PDF->SetY($position);
+        $PDF->SetX(125);
+        $PDF->MultiCell(60,8,utf8_decode($donne['montant']),1,'C');
+
+        $position += 8;
+
+        
         
         $PDF->Output();
         /*$recupNbCommade = $bdd->query("SELECT COUNT(*) AS nbCommade FROM commade WHERE userConnexion = '$user' ;");
