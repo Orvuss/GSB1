5fb13f575e450881c7154a089d108c5c85746536
Orvuss <77148361+Orvuss@users.noreply.github.com>
2021-10-14 12:11:08 +0200

    tg

--- GSB_AppliMVC/gsbMVC/vues/v_PDF.php
+++ GSB_AppliMVC/gsbMVC/vues/v_PDF.php
@@ -5,8 +5,7 @@
     $bdd = connexion();
     if($bdd){
         $user = $_SESSION['idVisiteur'];
-    
-      
+
         $PDF = new fpdf();
         $PDF->AddPage();
         $PDF->SetFont("Arial","B",16);
@@ -20,35 +19,26 @@
         $requete2->execute();
         $PDF->SetTextColor(0,0,0);
 
-        //Affichage des cell
-
-    
         $PDF->SetY($position-8);
         $PDF->SetX(15);
         $PDF->MultiCell(60,8,utf8_decode("Id du visiteur"),4,'C');
-
         $PDF->SetY($position-8);
         $PDF->SetX(45);
         $PDF->MultiCell(60,8,utf8_decode("Mois"),4,'C');
-
         $PDF->SetY($position-8);
         $PDF->SetX(75);
         $PDF->MultiCell(60,8,utf8_decode("Libellé"),4,'C');
-
         $PDF->SetY($position-8);
         $PDF->SetX(110);
         $PDF->MultiCell(60,8,utf8_decode("Date"),4,'C');
-
         $PDF->SetY($position-8);
         $PDF->SetX(140);
         $PDF->MultiCell(60,8,utf8_decode("Montant"),4,'C');
 
         $PDF->SetTextColor(0,0,0);
 
-
         $donne = $requete2->fetch();
-            
-        
+
         $id = $donne['id'];
         $select = $bdd->prepare("SELECT * FROM LigneFraisHorsForfait WHERE idVisiteur = :numero;");
         $select->bindParam(':numero', $user);
@@ -59,33 +49,22 @@
         $PDF->SetY($position);
         $PDF->SetX(15);
         $PDF->MultiCell(60,8,utf8_decode($donneesProduit['idVisiteur']),4,'C');
-
         $PDF->SetY($position);
         $PDF->SetX(45);
         $PDF->MultiCell(60,8,utf8_decode($donneesProduit['mois']),4,'C');
-
         $PDF->SetY($position);
         $PDF->SetX(75);
         $PDF->MultiCell(60,8,utf8_decode($donneesProduit['libelle']),4,'C');
-
         $PDF->SetY($position);
         $PDF->SetX(110);
         $PDF->MultiCell(60,8,utf8_decode($donneesProduit['date']),4,'C');
-
         $PDF->SetY($position);
         $PDF->SetX(140);
         $PDF->MultiCell(60,8,utf8_decode($donneesProduit['montant']),4,'C');
 
         $position += 8;
 
-        
-        
         $PDF->Output();
-        /*$recupNbCommade = $bdd->query("SELECT COUNT(*) AS nbCommade FROM commade WHERE userConnexion = '$user' ;");
-        $resultatNbCommade = $recupNbCommade->fetch();
-        $nbCommade = $resultatNbCommade['nbCommade'];
-        $nbCommade = $nbCommade + 1;
-        $PDF->Output("commande/".$user.$nbCommade.".PDF", "F");*/
     }
     else{
       echo "erreur";
